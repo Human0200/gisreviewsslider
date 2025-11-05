d4d6fc8d278b5c345d0021d219234bd19e48ebf3
Anton <ruant02@mail.ru>
2025-11-05 11:10:25 +0500

    правки

--- leadspace.gisreviewsslider/include.php
+++ leadspace.gisreviewsslider/include.php
@@ -5,7 +5,7 @@ use Bitrix\Main\ModuleManager;
 
 if (ModuleManager::isModuleInstalled('leadspace.gisreviewsslider') && 
     Loader::IncludeModule('leadspace.gisreviewsslider')) {
-    
+
     
     Loader::registerAutoLoadClasses('leadspace.gisreviewsslider', [
         "leadspace\gisreviewsslider\GisReviewsTable" => "lib/reviews.php",
